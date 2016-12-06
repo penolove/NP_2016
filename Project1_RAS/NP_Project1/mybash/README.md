66f0eef86ce7616dbd2afd99b8874a84a03d98ce
stream <penolove15@gmail.com>
2016-12-06 10:47:35 -0500

    lately writing project2

---
+++ Project1_RAS/NP_Project1/mybash/README.md
@@ -0,0 +1,7 @@
+make ;
+
+on server:
+./ras
+
+on client:
+nc 127.0.0.1 9081
--- Project1_RAS/NP_Project1/mybash/simple_shell.cpp
+++ Project1_RAS/NP_Project1/mybash/simple_shell.cpp
@@ -90,13 +90,16 @@ vector<CommandParse> commandParsing(char* command){
 					commands_v.push_back(temp_elem);
 					temp_elem= CommandParse();
 				}	
-			}else if(temp_str.length()==2)
+			}else if(temp_str.length()>=2)
 			{// 2 is |1 , |2 ,|3
 				//cout <<"[debug] n_pipe :"<<temp_str.c_str()[1]-'0'<<endl;
     				if(temp_elem.y.size()!=0)
 				{
 					temp_elem.x=2;
-					temp_elem.n_pipe=temp_str.c_str()[1]-'0';
+					for(int i=1;i<temp_str.length();i++){
+						temp_elem.n_pipe*=10;
+						temp_elem.n_pipe+=temp_str.c_str()[i]-'0';
+					}
 				}
 			}
 		}else if(temp_str[0]=='>'){
@@ -107,6 +110,19 @@ vector<CommandParse> commandParsing(char* command){
 				command_count+=1;
 				commands_v.push_back(temp_elem);
 				temp_elem= CommandParse();
+			}
+			//temp_elem = new CommandParse();	
+		}else if(temp_str[0]=='!'){
+			//3 is >
+			if(temp_str.length()>=2){
+				if(temp_elem.y.size()!=0)
+				{
+					temp_elem.x=4;
+					for(int i=1;i<temp_str.length();i++){
+						temp_elem.n_pipe*=10;
+						temp_elem.n_pipe+=temp_str.c_str()[i]-'0';
+					}
+				}
 			}
 			//temp_elem = new CommandParse();	
 		}else{
@@ -144,8 +160,85 @@ bool check_remains(N_Pipe_elemet value){
 	return value.queue_remains ==0;
 }
 
+bool sys_Check(vector<string>& sysOrder_v,int& order_size, char* argv[]){
+	vector<string>::iterator itr;
+	itr = find(sysOrder_v.begin(),sysOrder_v.end(),string(argv[0]));
+	//checkif is syscall
+	//cout<<"[debug] sysOrder :"<<itr-sysOrder_v.begin()<<endl;
+	//0 setenv , 1 printenv ,2 exit
+	switch(itr-sysOrder_v.begin()){
+		case 0:
+			if(order_size==3){
+				setenv(argv[1],argv[2],1);
+			}else{
+				cout<<"usage : setenv variable value"<<endl;
+			}
+			return 1;
+			break;
+		case 1:
+			if(order_size==2){
+				cout << argv[1] << "=" << getenv(argv[1]) << endl;	
+			}else{
+				cout<<"usage : printenv variable "<<endl;
+			}
+			return 1;
+			break;
+		case 2:
+			_exit(1);
+		default:
+			return 0;
+	}
+}
+
+void child_Fd_Handler(int& previous_x,int pipe_next[2],int pipe_curr[2],\
+	int& npipe_in,CommandParse curr_command, int& cur_cmd,\
+	vector<CommandParse>& commandParse_v){
+	//the pipe from privious command
+	if(previous_x==1 && pipe_next[0]>0){ // pipe from privious command
+		dup2(pipe_next[0],STDIN_FILENO);
+	}
+	if(npipe_in>0){   // for n_pipe stdin;
+
+		dup2(npipe_in,STDIN_FILENO);
+		//cout<<"[debug]there are once n pipe in : "<<npipe_in<<endl;	
+	}
+	if(curr_command.x==1 ){
+		//pipe to next command
+		dup2(pipe_curr[1],STDOUT_FILENO);
+		dup2(pipe_curr[1],STDERR_FILENO);
+
+	}else if(curr_command.x==2){
+		//pipe stdout to next command
+		dup2(pipe_curr[1],STDOUT_FILENO);
+
+	}else if(curr_command.x==4){
+		//pipe stderr/stdout to next command
+		dup2(pipe_curr[1],STDOUT_FILENO);
+		dup2(pipe_curr[1],STDERR_FILENO);
+
+	}else if(curr_command.x==3){
+		//pipe write to file
+		char * arg[1501];
+		int order_size_next;
+		str_vector2array((commandParse_v.at(cur_cmd+1)).y,arg,order_size_next);
+		
+		if(order_size_next!=1){
+			cout<<"usage : order > 123.txt "<<endl;
+			_exit(1);
+		}
+		FILE * pFile;
+	//	cout<<"[debug] the vector"<<(commandParse_v.at(cur_cmd+1)).y[0]<<endl;
+	//	cout<< "[debug] we ae writing to "<<arg[0]<<endl;
+		pFile = fopen (arg[0],"w");
+		dup2( fileno(pFile), STDOUT_FILENO);	
+		dup2( fileno(pFile), STDERR_FILENO);	
+	}
+
+}
+
 
 int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
+	cout<<endl;
 	int child_pid;
 	int cur_cmd=0;
 	int pipe_curr[2];
@@ -180,44 +273,26 @@ int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
 		//check setenv/printenv/exit(now it's still the main process)
 		char * argv[1501];
 		argv[0]=NULL;
-		str_vector2array((commandParse_v.at(cur_cmd)).y,argv,order_size);
-		itr = find(sysOrder_v.begin(),sysOrder_v.end(),string(argv[0]));
-		if(itr != sysOrder_v.end()){
-			//cout<<"[debug] sysOrder :"<<itr-sysOrder_v.begin()<<endl;
-			//0 setenv , 1 printenv ,2 exit
-			switch(itr-sysOrder_v.begin()){
-				case 0:
-					if(order_size==3){
-						setenv(argv[1],argv[2],1);
-					}else{
-						cout<<"usage : setenv variable value"<<endl;
-					}
-					break;
-				case 1:
-					if(order_size==2){
-						cout << argv[1] << "=" << getenv(argv[1]) << endl;	
-					}else{
-						cout<<"usage : printenv variable "<<endl;
-					}
-					break;
-				case 2:
-					_exit(1);
-			}	
+		CommandParse curr_command=commandParse_v.at(cur_cmd);
+
+		str_vector2array(curr_command.y,argv,order_size);
+
+		//checkif is syscall
+		if(sys_Check(sysOrder_v, order_size, argv)){
 			cur_cmd+=1;
 		}else{ 
 			//deal with pipe process x=1/n_pipe x=2
-			if(commandParse_v.at(cur_cmd).x<=2){
+			if(curr_command.x<=2||curr_command.x==4){
 				if(pipe(pipe_curr) < 0) {
 					cout << "[Client] Fail to create pipe\n";
 					return -1;
 				}
-				if(commandParse_v.at(cur_cmd).x==2){
+				if(curr_command.x==2||curr_command.x==4){
 					for(int i = 0; i < N_pipe_queue.size(); i ++) {
-						if(N_pipe_queue[i].queue_remains==commandParse_v.at(cur_cmd).n_pipe){
+						if(N_pipe_queue[i].queue_remains==curr_command.n_pipe){
 							//check if the target n command pipe has been created;
 							close(pipe_curr[1]);
 							pipe_curr[1]=N_pipe_queue[i].fd_in;
-							cout<<"[debug] the pipe already created : "<<pipe_curr[1]<<endl;
 							element_exist=1;
 						}
 					}
@@ -229,43 +304,11 @@ int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
 				printf("I fall my people\n");
 				_exit(1);
 			}else if(child_pid==0) {// children process
-				//the pipe from privious command
-				if(previous_x==1 && pipe_next[0]>0){ // pipe from privious command
-					dup2(pipe_next[0],STDIN_FILENO);
-				}
-				if(npipe_in>0){   // for n_pipe stdin;
-					dup2(npipe_in,STDIN_FILENO);
-				}
-				if(commandParse_v.at(cur_cmd).x==1){
-					//pipe to next command
-					dup2(pipe_curr[1],STDOUT_FILENO);
-					dup2(pipe_curr[1],STDERR_FILENO);
-
-				}else if(commandParse_v.at(cur_cmd).x==2){
-					//pipe to next n line command 
-					//here are lots of problems fuck 	
-					dup2(pipe_curr[1],STDOUT_FILENO);
-					dup2(pipe_curr[1],STDERR_FILENO);
-
-				}else if(commandParse_v.at(cur_cmd).x==3){
-					//pipe write to file
-	
-					char * arg[1501];
-					int order_size_next;
-					str_vector2array((commandParse_v.at(cur_cmd+1)).y,arg,order_size_next);
-					
-					if(order_size_next!=1){
-						cout<<"usage : order > 123.txt "<<endl;
-						_exit(1);
-					}
-					FILE * pFile;
-				//	cout<<"[debug] the vector"<<(commandParse_v.at(cur_cmd+1)).y[0]<<endl;
-				//	cout<< "[debug] we ae writing to "<<arg[0]<<endl;
-					pFile = fopen (arg[0],"w");
-					dup2( fileno(pFile), STDOUT_FILENO);	
-					dup2( fileno(pFile), STDERR_FILENO);	
-				}
-
+				 child_Fd_Handler( previous_x, pipe_next, pipe_curr,\
+					 npipe_in, curr_command,  cur_cmd, commandParse_v);
+				
+				//cout<<"[debug] command :"<<argv[0]<<endl;
+				//argv is the curr_command to char* array;
 				string x(argv[0]);
 				string curr_path;
 				if(fileAccessUnderPATH(x,F_OK,curr_path)){
@@ -280,18 +323,15 @@ int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
 					_exit(1);
 				}
 
+
 			}else{// the parent process
-				
-				// the npipe_in is for just the first command of targetline
-				npipe_in=0;
-				
 				//cout<< "[debug] Parent process: I'm In"<<endl;
-				int curr_act=commandParse_v.at(cur_cmd).x;
-				int pipe_target=commandParse_v.at(cur_cmd).n_pipe;
-				if(pipe_curr[1]!=0&&curr_act!=2){
+				npipe_in=0;
+				int curr_act=curr_command.x;
+				int pipe_target=curr_command.n_pipe;
+				if(pipe_curr[1]!=0&&(curr_act!=2&&curr_act!=4)){
 					close(pipe_curr[1]);
 				}
-
 				// actually is the pipe of pervious stdin
 				if(pipe_next[0]!=0){
 					close(pipe_next[0]);
@@ -300,19 +340,18 @@ int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
 					previous_x=1;
 					pipe_next[0]=pipe_curr[0];
 				}
-
 				//cout<< "[debug] Paren process:  we are waiting child"<<endl;
 				wait(&child_pid);
 				//cout<< "[debug] Parent process: the child is come"<<endl;
 				
-				//curr_act==3, write to file 				
+				//if write to file 				
 				if(curr_act==3){
 					cur_cmd+=1;
 				}
 				cur_cmd+=1;
 				if(cur_cmd>=commandParse_v.size()){
-					if(curr_act==2 && !element_exist){
-						cout<<"[debug] npipe to "<<pipe_curr[0]<<endl;
+					if((curr_act==2||curr_act==4) && !element_exist){
+						//cout<<"[debug] npipe to "<<pipe_curr[0]<<endl;
 						N_pipe_queue.push_back(N_Pipe_elemet(pipe_target,pipe_curr[0],pipe_curr[1]));	
 					}else{
 						if(pipe_curr[0]!=0){
@@ -323,8 +362,7 @@ int createProcess(char * filename, vector<N_Pipe_elemet>& N_pipe_queue){
 			}
 		}
 	}
-	
-	//clean all the pipe that already passed
+
 	N_pipe_queue.erase(remove_if(N_pipe_queue.begin(), N_pipe_queue.end(), check_remains),N_pipe_queue.end()); 
 
 }
@@ -337,12 +375,11 @@ int main(){
 	setenv("PATH", "./bin:.", 1); 
 	
 	vector<N_Pipe_elemet> N_pipe_queue;
-	//cout <<N_pipe_queue.size();
-	printf("#");
+	cout<<"%";
 	while((read=getline(&line,&len,stdin))!=-1){
 		line[read-1]='\0';
 		createProcess(line,N_pipe_queue);
-		printf("#");
+		cout<<"%";
 	}
         return 0;
 }
